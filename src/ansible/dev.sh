945225cc50c3e2361473acdec4cc2c4238572779
DarkStar <pegasus@quantasource.dev>
2025-05-29 04:26:10 -0500

    almost working! problem with GPG and sources.list

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -4,3 +4,6 @@ echo "*/" > roles/dev/.gitignore
 cd roles/dev
 git clone git@github.com:Unicorn-OS/ansible-role-Packer.git
 git clone git@github.com:Unicorn-OS/ansible-role-Packer-in-Podman.git
+git clone git@github.com:Unicorn-OS/ansible-role-Vagrant.git
+git clone git@github.com:Unicorn-OS/ansible-role-Vagrant-Libvirt.git
+git clone git@github.com:Unicorn-OS/ansible-role-Vagrant-LXD.git
